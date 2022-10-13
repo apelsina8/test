bfd6a875e647805d45223342cb9eb25277842497
apelsina8 <arhishow@gmail.com>
2022-10-13 22:39:09 +0300

    пуш

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+
+Console.Write("Input first number");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"My first number is {num}");
+*/
+
+
+
+// здесь я вывожу мое первое число
+//Задание 1. Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+//**Например:**
+
+//4 -> 16
+//-3 -> 9
+//-7 -> 49
+
+
+Console.WriteLine("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+Console.WriteLine($"Quad of {num} is -> {result}");
+
+345 -> 5
+345 % 10
+34*10
+5 
+
+
